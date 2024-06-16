8eb7e7a7923b8a3b873204877679680dcfc2dab5
MtJakhar <thvsd.10fingers@gmail.com>
2024-06-16 15:57:09 -0500

    chap 2 variables

---
+++ chapter2/variables.cpp
@@ -0,0 +1,17 @@
+//Muizz Jakhar June 16 2024
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int number = 7;
+  float equation = number / 2.0;
+  float equation2 = equation - 2;
+
+  cout << "Starting number = " << number << endl;
+  cout << "Divide by 2 = " << equation << endl;
+  cout << "Subtract 2 = " << equation2 << endl;
+
+  return 0;
+}
