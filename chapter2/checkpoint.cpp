3a8ecfb244361e524db14166034bda95cc65d4ac
MtJakhar <thvsd.10fingers@gmail.com>
2024-06-16 17:08:33 -0500

    checkpoint chap 2

---
+++ chapter2/checkpoint.cpp
@@ -0,0 +1,17 @@
+// Muizz Jakhar June 16 2024
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int number = 3;
+  float divideByTwo = number / 2.0;
+  float addThree = divideByTwo + 3;
+
+  cout << "Starting Number  = "  << number << endl;
+  cout << "Divide By Two  = "  << divideByTwo << endl;
+  cout << "Add Three  = "  << addThree << endl;
+
+  return 0;
+}
