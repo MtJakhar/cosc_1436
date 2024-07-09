91a5a30ae164bf00788e83596b985129675a0544
MtJakhar <thvsd.10fingers@gmail.com>
2024-07-09 11:30:44 -0500

    chap 6 done

---
+++ chapter6/checkpoint.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+bool divisibleBy3(int);
+
+int main () {
+  int userEntry;
+
+  for(int i = 0; i < 5; i++) {
+    cout << "Enter a number: "; 
+    cin >> userEntry;
+    if(divisibleBy3(userEntry) == true) {
+      cout << "Divisible by 3? true\n" << endl;
+    } else {
+      cout << "Divisible by 3? false\n" << endl;
+    }
+  }
+
+  return 0;
+}
+
+bool divisibleBy3(int number) {
+  if(number % 3 == 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
